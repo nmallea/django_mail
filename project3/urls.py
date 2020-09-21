2bd09afff4a7c6f56d22145ce1da82e1b494686c
Nikane Mallea <nikane@mac.com>
2020-09-21 15:21:18 +0200

    initial commit
    - project files

--- project3/urls.py
+++ project3/urls.py
@@ -1,4 +1,4 @@
-"""wiki URL Configuration
+"""project3 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("encyclopedia.urls"))
+    path('', include('mail.urls'))
 ]
